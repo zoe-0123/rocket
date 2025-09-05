4d4cfcb907d253e797c2cc12a9edc80442c07d60
zoe-0123 <leeck44@naver.com>
2025-09-05 13:48:17 +0900

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
 };
 
 export default nextConfig;
